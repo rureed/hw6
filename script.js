b3d65f22e8bf01cccf53d8cb9809ebe47ebc7aec
rureed <rureed@gmail.com>
2020-04-18 00:50:00 -0400

    setting functions, starting uv css

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ var searchBox
 var searchButton
 
 var apiKey = "a37e980e3eb1ba982a16a51051774957"
+var weatherUrl = "api.openweathermap.org/data/2.5/weather?q="
+
+
 
 function getHistory() {
     $("#history")
 }
+
+function addHistory() {
+    $("#history")
+}
+
+function getCurrent() {
+
+}
+
+function getForecast() {
+
+}
+
