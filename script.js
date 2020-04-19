247e174445727f74aa02f2fced7e3b56c48f1617
rureed <rureed@gmail.com>
2020-04-18 21:27:13 -0400

    chucking the forecast cards

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ var apiKey = "a37e980e3eb1ba982a16a51051774957";
 var forecast = "api.openweathermap.org/data/2.5/forecast?q=";
 var current = "api.openweathermap.org/data/2.5/weather?q=";
 var searchBox = $("#searchBox");
+var history = $("#history");
 
 
-// $(document).ready(function(){
+$(document).ready(function(){
     $("#searchBtn").on("click", function(){
-        current + $("#serchBox").val() + "&appid=" + apiKey;
-        forecast + $("#searchBox").val() + "&appid=" + apiKey;
+        var currentWeath = current + $("#searchBox").val() + "&appid=" + apiKey;
+        var currentForecast =  forecast + $("#searchBox").val() + "&appid=" + apiKey;
+console.log(currentWeath, currentForecast);
+
     })
-// })
-console.log(searchBox);
+})
 
 
 //     $("#searchBtn").click(function(){
