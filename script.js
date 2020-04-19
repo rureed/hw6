fb7b0b5cea17d625f4978506262c9bdbe4dd1ffd
rureed <rureed@gmail.com>
2020-04-18 20:44:22 -0400

    fixing numerous typos

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-var searchBox
-var searchButton
-
-var apiKey = "a37e980e3eb1ba982a16a51051774957"
-var weatherUrl = "api.openweathermap.org/data/2.5/weather?q="
-
+var apiKey = "a37e980e3eb1ba982a16a51051774957";
+var forecast = "api.openweathermap.org/data/2.5/forecast?q=";
+var current = "api.openweathermap.org/data/2.5/weather?q=";
+var searchBox = $("#searchBox");
 
 
-function getHistory() {
-    $("#history")
-}
+// $(document).ready(function(){
+    $("#searchBtn").on("click", function(){
+        current + $("#serchBox").val() + "&appid=" + apiKey;
+        forecast + $("#searchBox").val() + "&appid=" + apiKey;
+    })
+// })
+console.log(searchBox);
 
-function addHistory() {
-    $("#history")
-}
 
-function getCurrent() {
+//     $("#searchBtn").click(function(){
+//       $.ajax({url: weatherUrl + , success: function(result){
+//         $("#div1").html(result);
+//       }
+//     });
+//     });
+//   });
 
-}
+// function getHistory() {
+//     $("#history")
+// }
 
-function getForecast() {
+// function addHistory() {
+//     $("#history")
+// }
 
-}
+// function getCurrent() {
 
+// }
+
+// function getForecast() {
+
+// }
+
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
