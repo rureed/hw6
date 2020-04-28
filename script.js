2df67efe7a3b22c8ba85e34cdafd998bbccbd966
Russ Reed <rureed@gmail.com>
2020-04-28 16:29:00 -0400

    building forecast

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var apiKey = "a37e980e3eb1ba982a16a51051774957";
-var forecast = "api.openweathermap.org/data/2.5/forecast?q=" + $("#searchBox").val() + "&appid=" + apiKey;
+var forecast = "api.openweathermap.org/data/2.5/forecast?q=" + $("#searchBox").val() + "&units=imperial&appid=" + apiKey;
 var searchBox = $("#searchBox");
 var history = $("#history");
 var today = new Date();
@@ -19,7 +19,7 @@ console.log(currentWeather, currentForecast);
 })
 
 function showCurrentWeather(currentWeather){
-    var current = "http://api.openweathermap.org/data/2.5/weather?q=" + currentWeather + "&appid=" + apiKey;
+    var current = "http://api.openweathermap.org/data/2.5/weather?q=" + currentWeather + "&units=imperial&appid=" + apiKey;
     $.ajax({url: current, success: function(result){
         $("#todayArea").html(result);
         console.log(result)
@@ -48,7 +48,7 @@ function showCurrentWeather(currentWeather){
                 currentWeather.uvSeverity = "severe";
             else currentWeather.uvSeverity = "moderate";
         
-            var currentResult = $('<div class="card"><div class="card-body"><h1 class="card-title">' + currentWeather.city + ' ' + currentWeather.date + ' ' +
+            var currentResult = $('<div class="card"><div class="card-body" style="background-color: lightBlue"><h1 class="card-title">' + currentWeather.city + ' ' + currentWeather.date + ' ' +
             '<span class="badge"><img id="weather-icon" src="http://openweathermap.org/img/wn/' + currentWeather.currentIcon + '@2x.png"></span></h1>' +
             '<p class="card-text">Temperature: ' + currentWeather.temperature + ' °F</p>' +
             '<p class="card-text">Humidity: ' + currentWeather.humidity + '%</p>' +
@@ -61,8 +61,21 @@ console.log(uvResult, currentWeather.city)
 }
 
 function showForecast(){
+    var forecast = "api.openweathermap.org/data/2.5/forecast?q=" + $("#searchBox").val() + "&units=imperial&appid=" + apiKey;
+    var forecastArray = [];
     $.ajax({url: forecast, success: function(result){
         $("#forecastArea").html(result);
+        var forecastObject;
+        for (var i = 0; i < result.length; i++) {
+            forecastObject = {
+                date: result.list[i].dt_txt.split(" ")[0],
+                icon: result.list[i].weather[0].icon,
+                temperature: result.list[i].main.temp,
+                humidity: result.list[i].main.humidity
+            };
+        forecastArray.push(forecastObject);
+        }
+
     }})
 }
 
